6bcf765ebdcfa7093782b69074400976c285c37c
IloveColdCoffee <narek.manoukian0@gmail.com>
2022-08-29 10:33:25 +0400

    deleting all my page

--- src/pages/Narek/Narek.js
+++ src/pages/Narek/Narek.js
@@ -1,47 +1,4 @@
-import React, {useState} from 'react';
-import "./NareksCrud.css";
+import React from 'react';
 export default function Narek() {
-  
-    const [name, setName] = useState("")
-    const [allData, setAllData] = useState([])
-    const [show, setShow] = useState(false)
-    const[editIndex, setEditIndex] = useState()
-    const handleAdd=()=>{
-      if(name.length!==0){
-      setAllData(newData=>[... newData,name])
-      setName("")
-        }   
-    }
-    const handleDelete=(index)=>{
-      allData.splice(index, 1)
-      setAllData([...allData])
-    }
-    const handleEdit=(i)=>{
-      setName(allData[i])
-      setShow(true)
-      setEditIndex(i)
-    }
-    const handleUpdate=()=>{
-      allData.splice(editIndex,1,name)
-      setAllData([...allData])
-      setShow(false)
-    }
-  return (  
-      <div>
-        <input value={name} onChange={(e)=>setName(e.target.value)} />
-        {!show?<button onClick={handleAdd}>Add</button>:
-        <button onClick={handleUpdate}>Update</button>}
-        {
-          allData.map((val, i)=>
-            <div>
-              <h1>{val}</h1>
-              <button className='edit' onClick={()=>handleEdit(i)}>Edit</button>
-              <button className='delete' onClick={()=>handleDelete(i)}>Delete</button>
-            </div>
-            
-          )
-        }
-        
-      </div>
-  )
+  <div></div>
 }
