3c8867728188afb2461e2adf0ceebf6721f16d60
Henrik Hayrapetyan <henrikhayrapetyan91@gmail.com>
2022-09-03 17:23:12 +0400

    fix

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Login from './components/Login'
 import Menu from './components/Menu'
 import Henrik from './pages/Henrik/Henrik'
@@ -8,20 +8,19 @@ import Armine from './pages/Armine/Armine'
 import Nelly from './pages/Nelly/Nelly'
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Login />} />
-        <Route path='/menu' element={<Menu />} />
-        <Route path='/henrik' element={<Henrik />} />
-        <Route path='/nairi' element={<Nairi />} />
-        <Route path='/narek' element={<Narek />} />
-        <Route path='/armine' element={<Armine />} />
-        <Route path='/nelly' element={<Nelly />} />
-        
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<Login />} />
+                <Route path='/menu' element={<Menu />} />
+                <Route path='/henrik' element={<Henrik />} />
+                <Route path='/nairi' element={<Nairi />} />
+                <Route path='/narek' element={<Narek />} />
+                <Route path='/armine' element={<Armine />} />
+                <Route path='/nelly' element={<Nelly />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
